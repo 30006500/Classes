e88464f4363d58ecaa8831dd1af2a96418b08936
Campbell McChesney <30006500@g.toiohomai.ac.nz>
2018-08-02 09:52:54 +1200

    Ex1

---
+++ Object Class ex2/Object Class ex2/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Class_ex2
+{
+    class Person
+    {
+        string fname;
+        string lname;
+        double payrate;
+        double tax;
+        double accountnumber;
+
+        public Person(string _fname, string _lname, double _payrate, double _tax, double _accountnumber)
+        {
+            _fname = fname;
+            _lname = lname;
+            _payrate = payrate;
+            _tax = tax;
+            _accountnumber = accountnumber;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return fname;
+            }
+            set
+            {
+                fname = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lname;
+            }
+            set
+            {
+                lname = value;
+            }
+        }
+
+        public double Payrate
+        {
+            get
+            {
+                return payrate;
+            }
+            set
+            {
+                payrate = value;
+            }
+        }
+
+        public double Tax
+        {
+            get
+            {
+                return tax;
+            }
+            set
+            {
+                tax = value;
+            }
+        }
+
+        public double AccountNumber
+        {
+            get
+            {
+                return accountnumber;
+            }
+            set
+            {
+                accountnumber = value;
+            }
+        }
+    }
+}
---
+++ Object Class ex2/Object Class ex2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Class_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Person employee1 = new Person();
+            employee1.FirstName = "Campbell";
+            employee1.LastName = "McChesney";
+            employee1.Payrate = 35;
+            employee1.Tax = 22;
+            employee1.AccountNumber = 1234567890;
+
+            Person employee2 = new Person();
+            employee2.FirstName = "Alasdair";
+            employee2.LastName = "McChesney";
+            employee2.Payrate = 35;
+            employee2.Tax = 22;
+            employee2.AccountNumber = 0987654321;
+
+            Person employee3 = new Person();
+            employee3.FirstName = "Fiona";
+            employee3.LastName = "McChesney";
+            employee3.Payrate = 37;
+            employee3.Tax = 22;
+            employee3.AccountNumber = 13245768890;
+
+            Person employee4 = new Person();
+            employee4.FirstName = "Katrina";
+            employee4.LastName = "McChesney";
+            employee4.Payrate = 50;
+            employee4.Tax = 30;
+            employee4.AccountNumber = 27364536281;
+
+            Person employee5 = new Person();
+            employee5.FirstName = "Brooke";
+            employee5.LastName = "McChesney";
+            employee5.Payrate = 1;
+            employee5.Tax = 10;
+            employee5.AccountNumber = 736352538;
+            */
+            Person employee6 = new Person("John", "Murphy", 45, 22, 267876554);
+            Person employee7 = new Person("Debbie", "Littley", 45, 22, 746352628);
+            Person employee8 = new Person("Ruth", "McChesney", 45, 22, 8373538746);
+            Person employee9 = new Person("Mike", "Warring", 45, 22, 85734637372);
+            Person employee10 = new Person("Rhys", "Warring", 10, 10, 83487474272);
+        }
+    }
+}
